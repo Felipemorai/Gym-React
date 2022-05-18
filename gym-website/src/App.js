7292f54a6a1ee5e2598dbc0d098c9cb973220fd4
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-18 09:32:00 -0300

    header

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
