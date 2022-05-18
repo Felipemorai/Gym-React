7790e428f795ce0d52cc17be0e01c89404742cd4
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-18 15:47:46 -0300

    features

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar';
 import Header from "./Components/Header";
+import Feature from "./Components/Feature";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <Feature />
     </div>
   );
 }
