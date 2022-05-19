ed6336f9d3a2e2d45dc786e71a812370bd0f1552
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-19 09:11:54 -0300

    title

--- gym-website/src/Components/Feature.jsx
+++ gym-website/src/Components/Feature.jsx
@@ -10,10 +10,10 @@ function Feature() {
     <div id="features">
         <h1>FEATURES</h1>
         <div className="a-container">
-            <Featurebox image={fimage1} title="" />
-            <Featurebox image={fimage2} title="" />
-            <Featurebox image={fimage3} title="" />
-            <Featurebox image={fimage4} title="" />
+            <Featurebox image={fimage1} title="WeightLifting" />
+            <Featurebox image={fimage2} title="Specific Muscle" />
+            <Featurebox image={fimage3} title="Flex Your Muscle" />
+            <Featurebox image={fimage4} title="Cardio Exercise" />
         </div>
     </div>
   )
