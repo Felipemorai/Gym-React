25f5f275685198577a4ca0d9dadbfe381651bdfe
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-20 08:51:35 -0300

    presentation

--- gym-website/src/Components/Offer.jsx
+++ gym-website/src/Components/Offer.jsx
@@ -5,10 +5,9 @@ function Offer() {
     <div id="offer">
         <div className="pr-heading">
             <h1>A BIG <span>OFFER</span> FOR THIS SUMMER</h1>
-            <p className="details">Sign up for a 1 year membership now,
-            and we'll give you a bonus!</p>
+            <p className="details">Sign up for a 1 year membership now, and we'll give you a bonus! </p>
             <div className="pr-btns">
-                <a href="#" className="pr-btn">JOIN NOW</a>
+                <a href="#" className='pr-btn'>SIGN UP</a>
             </div>
         </div>
     </div>
