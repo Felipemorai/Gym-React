6bbbce883cda3ec7f845a7ce03ddb69702699d36
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-20 09:16:48 -0300

    about

--- gym-website/src/App.js
+++ gym-website/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar';
 import Header from "./Components/Header";
 import Feature from "./Components/Feature";
 import Offer from "./Components/Offer";
+import About from "./Components/About";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Feature />
       <Offer />
+      <About />
     </div>
   );
 }
---
+++ gym-website/src/Components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import aboutimage from '../images/about.png';
+
+function About() {
+    return (
+        <div id='about'>
+            <div className='about-image'>
+                <img src={aboutimage} alt='aboutimage' />
+            </div>
+            <div className='about-text'>
+                <h1>LEARN MORE ABOUT US</h1>
+                <p>We're more than just a gym. We are family.
+                    We encourage, we empower, we guide,
+                    we belive, we push, we strengthen.
+                   We are here to help you achieve your goals.
+                    At Fitness & Beyond, our goal is simple:
+                   Let us help you <span class="up">Find Your Strength!</span>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About;
