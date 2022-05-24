de449ae5bacbba1a684b51e0e7dd8c990651f68f
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2022-05-24 09:11:45 -0300

    animation

--- gym-website/src/Components/Navbar.jsx
+++ gym-website/src/Components/Navbar.jsx
@@ -17,7 +17,7 @@ function Navbar() {
 
     return (
         <nav className={nav ? "nav active" : "nav"}>
-            <Link to="#" className="logo">
+            <Link to="main" className="logo" smooth={true} duration={2000}>
                 <img src={logo} alt="" />
             </Link>
             <input className="menu-btn" type="checkbox" id="menu-btn" />
@@ -25,11 +25,11 @@ function Navbar() {
                 <span className="nav-icon"></span>
             </label>
             <ul className="menu">
-                <li><Link to="#">Header</Link></li>
-                <li><Link to="#">Features</Link></li>
-                <li><Link to="#">Offer</Link></li>
-                <li><Link to="#">About</Link></li>
-                <li><Link to="#">Contact</Link></li>
+                <li><Link to="main" smooth={true} duration={1000}>Header</Link></li>
+                <li><Link to="features" smooth={true} duration={1000}>Features</Link></li>
+                <li><Link to="offer" smooth={true} duration={1000}>Offer</Link></li>
+                <li><Link to="about" smooth={true} duration={1000}>About</Link></li>
+                <li><Link to="contact" smooth={true} duration={1000}>Contact</Link></li>
             </ul>
         </nav>
     );
